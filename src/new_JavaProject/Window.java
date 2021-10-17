884e133c0d258cdd1c26e95790e8a9fd9c7abe62
leeg0m <hennurunge@naver.com>
2021-10-17 16:59:37 +0900

    Initial commit

---
+++ src/new_JavaProject/Window.java
@@ -0,0 +1,346 @@
+package new_JavaProject;
+
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Window {
+
+	private JFrame frame;
+	private int mouseX, mouseY;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window window = new Window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setUndecorated(true);
+
+		// 게임 종료 버튼
+		JButton exitButton = new JButton("");
+		exitButton.setLocation(1248, 7);
+		exitButton.setSize(21, 20);
+		exitButton.setIcon(new ImageIcon(".\\images\\exitButton.png"));
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.getContentPane().add(exitButton);
+
+		// 게임 메뉴바(menuBar)
+		JLabel menuBar = new JLabel("");
+		menuBar.setIcon(new ImageIcon(".\\images\\menuBar.png"));
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				frame.setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		frame.getContentPane().add(menuBar);
+
+		// 게임시작화면
+		ImagePanel startGamePage = new ImagePanel(
+				new ImageIcon(".\\images/startGamePage.png")
+						.getImage());
+		frame.setSize(startGamePage.getWidth(), startGamePage.getHeight());
+
+		// 로그인화면
+		ImagePanel loginPage = new ImagePanel(
+				new ImageIcon("./images/mainmenuPage.png").getImage());
+		frame.setSize(loginPage.getWidth(), loginPage.getHeight());
+
+		frame.getContentPane().add(loginPage);
+
+		
+
+		//로그인화면 버튼들
+		JButton loginButton = new JButton("\uC784\uC2DC\uB85C\uAE34");
+		loginButton.setBounds(402, 456, 97, 108);
+		loginButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				loginPage.setVisible(false);
+				startGamePage.setVisible(true);
+
+			}
+		});
+		loginPage.add(loginButton);
+
+		JButton signupButton = new JButton("\uC784\uC2DC\uD68C\uC6D0\uAC11");
+		signupButton.setBounds(651, 415, 97, 142);
+		loginPage.add(signupButton);
+
+		frame.getContentPane().add(startGamePage);
+
+		// 랭크화면
+		ImagePanel rankPage = new ImagePanel(
+				new ImageIcon(".\\images\\rankPage.png").getImage());
+		frame.setSize(rankPage.getWidth(), rankPage.getHeight());
+		frame.getContentPane().add(rankPage);
+
+		// 튜토리얼화면
+		ImagePanel htpPage = new ImagePanel(
+				new ImageIcon(".\\images/htpPage.png").getImage());
+		frame.setSize(htpPage.getWidth(), htpPage.getHeight());
+		frame.getContentPane().add(htpPage);
+		htpPage.setVisible(false);
+		rankPage.setVisible(false);
+		startGamePage.setVisible(false);
+		frame.getContentPane().setLayout(null);
+
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		
+		//게임시작화면 버튼들
+		JButton startGamePage_singleButton = new JButton("\uC784\uC2DC");
+		startGamePage_singleButton.setBounds(207, 399, 97, 23);
+		startGamePage.add(startGamePage_singleButton);
+
+		JButton startGamePage_multiButton = new JButton("\uC784\uC2DC");
+		startGamePage_multiButton.setBounds(633, 399, 97, 23);
+		startGamePage.add(startGamePage_multiButton);
+
+		JButton startGamePage_rankButton = new JButton("");
+		startGamePage_rankButton.setBounds(244, 59, 210, 40);
+		startGamePage_rankButton.setBorderPainted(false);
+		startGamePage_rankButton.setContentAreaFilled(false);
+		startGamePage_rankButton.setFocusPainted(false);
+		startGamePage_rankButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				startGamePage.setVisible(false);
+				rankPage.setVisible(true);
+			}
+		});
+		startGamePage.add(startGamePage_rankButton);
+
+		JButton startGamePage_htpButton = new JButton("");
+		startGamePage_htpButton.setBounds(466, 59, 210, 40);
+		startGamePage_htpButton.setBorderPainted(false);
+		startGamePage_htpButton.setContentAreaFilled(false);
+		startGamePage_htpButton.setFocusPainted(false);
+		startGamePage_htpButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				startGamePage.setVisible(false);
+				htpPage.setVisible(true);
+			}
+		});
+		startGamePage.add(startGamePage_htpButton);
+		
+		
+		//랭크화면 버튼들
+		JButton rankPage_startGameButton = new JButton("");
+		rankPage_startGameButton.setBounds(26, 57, 210, 40);
+		rankPage_startGameButton.setBorderPainted(false);
+		rankPage_startGameButton.setContentAreaFilled(false);
+		rankPage_startGameButton.setFocusPainted(false);
+		rankPage_startGameButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				rankPage.setVisible(false);
+				startGamePage.setVisible(true);
+			}
+		});
+		rankPage.add(rankPage_startGameButton);
+
+		JButton rankPage_htpButton = new JButton("");
+		rankPage_htpButton.setBounds(466, 57, 210, 40);
+		rankPage_htpButton.setBorderPainted(false);
+		rankPage_htpButton.setContentAreaFilled(false);
+		rankPage_htpButton.setFocusPainted(false);
+		rankPage_htpButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				rankPage.setVisible(false);
+				htpPage.setVisible(true);
+			}
+		});
+		rankPage.add(rankPage_htpButton);
+
+		//튜토리얼화면 버튼들
+		JButton htpPage_startGameButton = new JButton("");
+		htpPage_startGameButton.setBounds(29, 59, 210, 40);
+		htpPage_startGameButton.setBorderPainted(false);
+		htpPage_startGameButton.setContentAreaFilled(false);
+		htpPage_startGameButton.setFocusPainted(false);
+		htpPage_startGameButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				htpPage.setVisible(false);
+				startGamePage.setVisible(true);
+			}
+		});
+		htpPage.add(htpPage_startGameButton);
+
+		JButton htpPage_rankButton = new JButton("");
+		htpPage_rankButton.setBounds(251, 59, 210, 40);
+		htpPage_rankButton.setBorderPainted(false);
+		htpPage_rankButton.setContentAreaFilled(false);
+		htpPage_rankButton.setFocusPainted(false);
+		htpPage_rankButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				htpPage.setVisible(false);
+				rankPage.setVisible(true);
+			}
+		});
+		htpPage.add(htpPage_rankButton);
+
+	}
+}
+
+class ImagePanel extends JPanel {
+	private Image img;
+
+	public ImagePanel(Image img) {
+		this.img = img;
+		setSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+		setPreferredSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+		setLayout(null);
+	}
+
+	public int getWidth() {
+		return img.getWidth(null);
+	}
+
+	public int getHeight() {
+		return img.getHeight(null);
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+	}
+}
