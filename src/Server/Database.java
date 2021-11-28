5df49cb8f11c5ab3948c958a52f008e6b666b241
손영진 <qazwxxedc762@naver.com>
2021-11-28 21:49:19 +0900

    211228_pm09:49_update

--- src/Server/Database.java
+++ src/Server/Database.java
@@ -6,8 +6,8 @@ import java.sql.*;
 
 public class Database {
     /* 데이터베이스와의 연결에 사용할 변수들 */
-    Connection con = null; // 연결
-    Statement stmt = null; // 상태
+    Connection conn = null; // 연결
+    Statement state = null; // 상태
     String url = "jdbc:mariadb//localhost:3306/serverTimezone=Asia/Seoul";
     String user = "계정 이름";
     String passwd = "계정 비밀번호";
@@ -16,8 +16,8 @@ public class Database {
         try {	//데이터베이스 연결은 try-catch문으로 예외를 잡아준다.
             //데이터베이스와 연결한다.
             Class.forName("com.mariadb.jdbc.Driver");
-            con = DriverManager.getConnection(url, user, passwd);
-            stmt = con.createStatement();
+            conn = DriverManager.getConnection(url, user, passwd);
+            state = conn.createStatement();
             System.out.println("[Server] mariaDB 서버 연동 성공");	//데이터베이스 연결에 성공하면 성공을 콘솔로 알린다.
         } catch(Exception e) {	//데이터베이스 연결에 예외가 발생했을 때 실패를 콘솔로 알린다.
             System.out.println("[Server] mariaDB 서버 연동 실패> " + e.toString());
@@ -35,7 +35,7 @@ public class Database {
         try {
             //id와 일치하는 비밀번호와 닉네임이 있는지 조회한다.
             String checkingStr = "SELECT password, nickname FROM member WHERE id='" + id + "'";
-            ResultSet result = stmt.executeQuery(checkingStr);
+            ResultSet result = state.executeQuery(checkingStr);
 
             int count = 0;
             while(result.next()) {
@@ -59,21 +59,18 @@ public class Database {
         return nickname;	//nickname 반환
     }
 
-    //회원가입을 수행하는 메소드! 회원가입 성공 → true, 실패 → false
-    boolean joinCheck(String _n, String _nn, String _i, String _p, String _e) {
-        boolean flag = false;	//참거짓을 반환할 flag 변수. 초기값은 false
+    //회원가입을 수행하는 메소드. 성공하면 true, 실패하면 false
+    boolean signupCheck(String _nickname, String _id, String _password) {
+        boolean flag = false; // 참 거짓을 반환나는 flag 변수, 초기값은 false
 
-        //매개변수로 받은 각 문자열들을 각 변수에 초기화
-        String na = _n;
-        String nn = _nn;
-        String id = _i;
-        String pw = _p;
-        String em = _e;
+        String user_nickname = _nickname;
+        String user_id = _id;
+        String user_password = _password;
 
         try {
-            //member 테이블에 각 문자열들을 순서대로 업데이트하는 문장. 0승 0패, 1000점 으로 초기화
-            String insertStr = "INSERT INTO member VALUES('" + na + "', '" + nn + "', '" + id + "', '" + pw + "', '" + em + "', 0, 0,1000)";
-            stmt.executeUpdate(insertStr);
+            //member 테이블에 각 문자열들을 순서대로 업데이트하는 문장. 승, 패는 초기값을 숫자 0으로 한다.
+            String insertStr = "INSERT INTO member VALUES('" + user_nickname + "', '" + user_id + "', '" + user_password + "', 0,0)";
+            state.executeUpdate(insertStr);
 
             flag = true;	//업데이트문이 정상적으로 수행되면 flag를 true로 초기화하고 성공을 콘솔로 알린다.
             System.out.println("[Server] 회원가입 성공");
@@ -81,41 +78,38 @@ public class Database {
             flag = false;
             System.out.println("[Server] 회원가입 실패 > " + e.toString());
         }
-
         return flag;	//flag 반환
     }
 
-    //아이디 or 닉네임 중복 체크 메소드! 중복값 존재o → false, 존재x → true를 반환한다.
+    //넥네임 이나 아이디 가 중복되었는지 확인해주는 메소드 , 중복이면 false 중복이지 않으면 true
     boolean overCheck(String _a, String _v) {
-        boolean flag = false;	//참거짓을 반환할 flag 변수. 초기값은 false
+        boolean flag =false; // 참거짓, 초기값이 false
 
-        //att(attribute)는 속성(아이디, 닉네임)을 구분하고, val(value)은 확인할 값이 초기화.
-        String att = _a;
-        String val = _v;
+        String attribute = _a; //속성 - 아이디 비번
+        String val = _v; // 확인할 값이 초기화 ?? 무스말인지 몰겠음
 
         try {
-            //member 테이블에 존재하는 아이디(혹은 닉네임)를 모두 찾는다.
-            String selcectStr = "SELECT " + att + " FROM member";
-            ResultSet result = stmt.executeQuery(selcectStr);
+            //member 테이블에 존재하는 아이디 or 닉네임을 모두 찾는다
+
+            String selectStr = "SELECT" + attribute + "FROM member ";
+            ResultSet result = state.executeQuery(selectStr);
 
             int count = 0;
-            while(result.next()) {
-                //조회한 아이디(혹은 닉네임)과 val을 비교.
-                if(!val.equals(result.getString(att))) {	//val과 같은 것이 존재하면 flag를 true로 변경한다.
-                    flag = true;
-                }
 
-                else {	//val과 같은 것이 존재하지 않으면 flag를 false로 변경한다.
+            while (result.next() ) {
+                //조회한 아이디 or 닉네임과 val을 비교
+                if (val.equals(result.getString(attribute))) {  //val 과 같은 것이 있다면 flag값을 true 로 변경
+                    flag = true;
+                } else { //val 과 갑튼 것이 없다면 false로 변경함
                     flag = false;
                 }
                 count++;
             }
-            System.out.println("[Server] 중복 확인 성공");	//정상적으로 수행되었을 때 성공을 콘솔로 알린다.
-        } catch(Exception e) {	//정상적으로 수행하지 못하면 실패를 콘솔로 알린다.
-            System.out.println("[Server] 중복 확인 실패 > " + e.toString());
+            System.out.println("[SERVER] 중복 확인 성공! > " );
+        } catch (Exception e) {
+            System.out.println("[SERVER] 중복 확인 실패! > " + e.toString());
         }
-
-        return flag;	//flag 반환
+        return flag;
     }
 
     //DB에 저장된 자신의 정보를 조회하는 메소드! 조회한 정보들을 String 형태로 반환
@@ -128,7 +122,7 @@ public class Database {
         try {
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 이름과 이메일 정보를 조회한다.
             String viewStr = "SELECT name, email FROM member WHERE nickname='" + nick + "'";
-            ResultSet result = stmt.executeQuery(viewStr);
+            ResultSet result = state.executeQuery(viewStr);
 
             int count = 0;
             while(result.next()) {
@@ -144,7 +138,7 @@ public class Database {
         return msg;	//msg 반환
     }
 
-    //회원정보를 변경을 수행하는 메소드! 변경에 성공하면 true, 실패하면 false를 반환한다.
+/*    //회원정보를 변경을 수행하는 메소드! 변경에 성공하면 true, 실패하면 false를 반환한다.
     boolean changeInfo(String _nn, String _a, String _v) {
         boolean flag = false;	//참거짓을 반환할 flag 변수. 초기값은 false.
 
@@ -156,7 +150,7 @@ public class Database {
         try {
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 att(이름, 이메일, 비밀번호)를 val로 변경한다.
             String changeStr = "UPDATE member SET " + att + "='" + val + "' WHERE nickname='" + nick +"'";
-            stmt.executeUpdate(changeStr);
+            state.executeUpdate(changeStr);
 
             flag = true;	//정상적으로 수행되면 flag를 true로 바꾸고 성공을 콘솔로 알린다.
             System.out.println("[Server] 회원정보 변경 성공");
@@ -166,6 +160,7 @@ public class Database {
         }
         return flag;	//flag 반환
     }
+*/
 
     //전체 회원의 전적을 조회하는 메소드. 모든 회원의 전적을 String 형태로 반환한다.
     String viewRank() {
@@ -174,7 +169,7 @@ public class Database {
         try {
             //member 테이블의 닉네임, 승, 패를 모두 조회한다.
             String viewStr = "SELECT nickname, win, lose FROM member";
-            ResultSet result = stmt.executeQuery(viewStr);
+            ResultSet result = state.executeQuery(viewStr);
 
             int count = 0;
             while(result.next()) {
@@ -190,6 +185,7 @@ public class Database {
         return msg;	//msg 반환
     }
 
+    /*
     //한 명의 회원의 전적을 조회하는 메소드. 해당 회원의 전적을 String 형태로 반환한다.
     String searchRank(String _nn) {
         String msg = "null";	//전적을 받을 문자열. 초기값은 "null"로 한다.
@@ -200,7 +196,7 @@ public class Database {
         try {
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 승, 패를 조회한다.
             String searchStr = "SELECT win, lose FROM member WHERE nickname='" + nick + "'";
-            ResultSet result = stmt.executeQuery(searchStr);
+            ResultSet result = state.executeQuery(searchStr);
 
             int count = 0;
             while(result.next()) {
@@ -215,6 +211,7 @@ public class Database {
 
         return msg;	//msg 반환
     }
+    */
 
     //게임 승리 시 전적을 업데이트하는 메소드! 조회 및 업데이트 성공 → true, 실패 → false 반환
     boolean winRecord(String _nn) {
@@ -227,7 +224,7 @@ public class Database {
         try {
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 승리 횟수를 조회한다.
             String searchStr = "SELECT win FROM member WHERE nickname='" + nick + "'";
-            ResultSet result = stmt.executeQuery(searchStr);
+            ResultSet result = state.executeQuery(searchStr);
 
             int count = 0;
             while(result.next()) {
@@ -239,7 +236,7 @@ public class Database {
 
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 승리 횟수를 num으로 업데이트한다.
             String changeStr = "UPDATE member SET win=" + num + " WHERE nickname='" + nick +"'";
-            stmt.executeUpdate(changeStr);
+            state.executeUpdate(changeStr);
             flag = true;	//조회 및 업데이트 성공 시 flag를 true로 바꾸고 성공을 콘솔로 알린다.
             System.out.println("[Server] 전적 업데이트 성공");
         } catch(Exception e) {	//조회 및 업데이트 실패 시 flag를 false로 바꾸고 실패를 콘솔로 알린다.
@@ -261,7 +258,7 @@ public class Database {
         try {
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 패배 횟수를 조회한다.
             String searchStr = "SELECT lose FROM member WHERE nickname='" + nick + "'";
-            ResultSet result = stmt.executeQuery(searchStr);
+            ResultSet result = state.executeQuery(searchStr);
 
             int count = 0;
             while(result.next()) {
@@ -273,7 +270,7 @@ public class Database {
 
             //member 테이블에서 nick이라는 닉네임을 가진 회원의 승리 횟수를 num으로 업데이트한다.
             String changeStr = "UPDATE member SET lose=" + num + " WHERE nickname='" + nick +"'";
-            stmt.executeUpdate(changeStr);
+            state.executeUpdate(changeStr);
             flag = true;	//조회 및 업데이트 성공 시 flag를 true로 바꾸고 성공을 콘솔로 알린다.
             System.out.println("[Server] 전적 업데이트 성공");
         } catch(Exception e) {	//조회 및 업데이트 실패 시 flag를 false로 바꾸고 실패를 콘솔로 알린다.
